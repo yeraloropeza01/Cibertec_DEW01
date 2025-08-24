27b0eaaec57bdc31e04823d9daf49eead7df9854
Gerber Pacheco <gerber.pacheco@gerware.com>
2025-08-23 19:00:36 -0500

    avance en clase

--- trabajofinal/src/componentes/MainElige.jsx
+++ trabajofinal/src/componentes/MainElige.jsx
@@ -1,14 +1,18 @@
 import "./MainElige.css"
-import Volkswagen from '../imagenes/volkswagen.png';
-import MercedezBenz from '../imagenes/mercedez.png';
-import Nisan from '../imagenes/nissan.png';
-import Mazda from '../imagenes/mazda.png';
-import Honda from '../imagenes/honda.png';
-import Lexus from '../imagenes/lexus.png';
-import Lamborghini from '../imagenes/lamboghini.png';
-import BMW from '../imagenes/bmw.png';
 
 function ParteMainElige (){
+  
+    const marcas = [
+      {nombre: "Volkswager", imagen: "volkswagen.png"},
+      {nombre: "Mercedes", imagen: "mercedez.png"},
+      {nombre: "nissan", imagen: "nissan.png"},
+      {nombre: "mazda", imagen: "mazda.png"},
+      {nombre: "honda", imagen: "honda.png"},
+      {nombre: "lexus", imagen: "lexus.png"},
+      {nombre: "lambohini", imagen: "lamboghini.png"},
+      {nombre: "bwm", imagen: "bmw.png"}
+      
+    ]
     return(
     <main class="mainM">
         <div>
@@ -16,72 +20,21 @@ function ParteMainElige (){
           <section className="sectionM">
             <article className="articuloM">
               <div className="unoM">
-                <div className="articulo1M">
+                {
+                  marcas.map((m) => {
+                    return (
+                    <div className="articulo1M">
                     <div className="imagenM">
-                      <img src={Volkswagen} alt=""/>
+                      <img src={`/marcas/${m.imagen}`} alt=""/>
                     </div>
                     <button className='botonM'>
-                      <span>Volkswagen</span>
+                      <span>{m.nombre}</span>
                     </button>
-                </div>
-                <div className="articulo1M">
-                    <div className="imagenM">
-                      <img src={MercedezBenz} alt=""/>
-                    </div>
-                    <button className='botonM'>
-                      <span>MercedezBenz</span>
-                    </button>
-                </div>
-                <div className="articulo1M">
-                    <div className="imagenM">
-                      <img src={Nisan} alt=""/>
-                    </div>
-                    <button className='botonM'>
-                      <span>Nisan</span>
-                    </button>
-                </div>
-                <div className="articulo1M">
-                    <div className="imagenM">
-                      <img src={Mazda} alt=""/>
-                    </div>
-                    <button className='botonM'>
-                      <span>Mazda</span>
-                    </button>
-                </div>
-              </div>
-              <div className="dosM">
-                <div className="articulo2M">
-                    <div className="imagenM">
-                      <img src={Honda} alt=""/>
-                    </div>
-                    <button className='botonM'>
-                      <span>Honda</span>
-                    </button>
-                </div>
-                <div className="articulo2M">
-                    <div className="imagenM">
-                      <img src={Lexus} alt=""/>
-                    </div>
-                    <button className='botonM'>
-                      <span>Lexus</span>
-                    </button>
-                </div>
-                <div className="articulo2M">
-                    <div className="imagenM">
-                      <img src={Lamborghini} alt=""/>
-                    </div>
-                    <button className='botonM'>
-                      <span>Lamborghini</span>
-                    </button>
-                </div>
-                <div className="articulo2M">
-                    <div className="imagenM">
-                      <img src={BMW} alt=""/>
-                    </div>
-                    <button className='botonM'>
-                      <span>BMW</span>
-                    </button>
-                </div>
+                    </div>)
+                  } ) 
+                }
+
+                
               </div>
             </article>
           </section>

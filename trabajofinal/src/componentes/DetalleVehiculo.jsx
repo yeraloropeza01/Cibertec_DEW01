0cb0f2a3fded2380949c8856cfa7a96f805a491d
yerald <yeraloropeza01@gmail.com>
2025-08-27 00:56:27 -0500

    carritos2

--- trabajofinal/src/componentes/DetalleVehiculo.jsx
+++ trabajofinal/src/componentes/DetalleVehiculo.jsx
@@ -4,10 +4,16 @@ function DetalleVehiculo({ vehiculo, onVolver }) {
   return (
     <div className="detalle-card">
       <button className="detalle-volver" onClick={onVolver}>⬅ Volver</button>
-      <img className="detalle-imagen" src={vehiculo.imagen} alt={vehiculo.modelo} />
+      <img className="detalle-imagen" src={vehiculo.imagen2} alt={vehiculo.modelo} />
       <h2>{vehiculo.marca} {vehiculo.modelo}</h2>
       <p><strong>Año:</strong> {vehiculo.año}</p>
       <p><strong>Precio:</strong> ${vehiculo.precio}</p>
+      <p><strong>Motor:</strong> {vehiculo.motor}</p>
+      <p><strong>Transmision:</strong> {vehiculo.transmision}</p>
+      <p><strong>Combustible:</strong> {vehiculo.combustible}</p>
+      <p><strong>Color:</strong> {vehiculo.color}</p>
+      <p><strong>Puertas:</strong> {vehiculo.puertas}</p>
+      <p><strong>Potencia:</strong> {vehiculo.potencia}</p>
     </div>
   );
 }
--- trabajofinal/src/componentes/Galeria.jsx
+++ trabajofinal/src/componentes/Galeria.jsx
@@ -7,29 +7,29 @@ function Galeria({ marca }) {
 
   // lista de vehículos (agrega/edita según tus imágenes en /public/modelos/)
   const vehiculos = [
-    { marca: "Volkswagen", modelo: "Golf", año: 2021, precio: 18000, imagen: "/modelos/1volkswagenLateral.png", motor: "1.4L TSI", transmision: "DSG 7 vel.", combustible: "Gasolina", color: "Rojo", puertas: 5, potencia: "150 HP" },
-    { marca: "Volkswagen", modelo: "Passat", año: 2019, precio: 20000, imagen: "/modelos/1VOLKSWAGENPASAT LATERAL.png", motor: "2.0L TSI", transmision: "DSG 6 vel.", combustible: "Gasolina", color: "Negro", puertas: 4, potencia: "190 HP" },
+    { marca: "Volkswagen", modelo: "Golf", año: 2021, precio: 18000, imagen2: "/modeloU/1volkswagen.png" ,imagen: "/modelos/1volkswagenLateral.png", motor: "1.4L TSI", transmision: "DSG 7 vel.", combustible: "Gasolina", color: "Rojo", puertas: 5, potencia: "150 HP" },
+    { marca: "Volkswagen", modelo: "Passat", año: 2019, precio: 20000, imagen2: "/modeloU/1VOLKSWAGENPASAT.png", imagen: "/modelos/1VOLKSWAGENPASAT LATERAL.png", motor: "2.0L TSI", transmision: "DSG 6 vel.", combustible: "Gasolina", color: "Negro", puertas: 4, potencia: "190 HP" },
 
-    { marca: "Mercedes", modelo: "Clase A", año: 2022, precio: 30000, imagen: "/modelos/2MBClaseALateral.png", motor: "1.3L Turbo", transmision: "Automática 7 vel.", combustible: "Gasolina", color: "Plata", puertas: 5, potencia: "163 HP" },
-    { marca: "Mercedes", modelo: "Clase C", año: 2020, precio: 35000, imagen: "/modelos/2MBClaseClateral.png", motor: "2.0L Turbo", transmision: "Automática 9 vel.", combustible: "Gasolina", color: "Blanco", puertas: 4, potencia: "255 HP" },
+    { marca: "Mercedes", modelo: "Clase A", año: 2022, precio: 30000, imagen2: "/modeloU/2MBClaseA.png", imagen: "/modelos/2MBClaseALateral.png", motor: "1.3L Turbo", transmision: "Automática 7 vel.", combustible: "Gasolina", color: "Plata", puertas: 5, potencia: "163 HP" },
+    { marca: "Mercedes", modelo: "Clase C", año: 2020, precio: 35000, imagen2: "/modeloU/2MBClaseC.png", imagen: "/modelos/2MBClaseClateral.png", motor: "2.0L Turbo", transmision: "Automática 9 vel.", combustible: "Gasolina", color: "Blanco", puertas: 4, potencia: "255 HP" },
 
-    { marca: "Nissan", modelo: "Sentra", año: 2021, precio: 17000, imagen: "/modelos/3nisancentraLateral.png", motor: "1.6L", transmision: "Automática CVT", combustible: "Gasolina", color: "Gris", puertas: 4, potencia: "122 HP" },
-    { marca: "Nissan", modelo: "Altima", año: 2021, precio: 17000, imagen: "/modelos/3nissanaltimaLateral.png", motor: "1.6L", transmision: "Automática CVT", combustible: "Gasolina", color: "Gris", puertas: 4, potencia: "122 HP" },
+    { marca: "Nissan", modelo: "Sentra", año: 2021, precio: 17000, imagen2: "/modeloU/3nisancentraa.png", imagen: "/modelos/3nisancentraLateral.png", motor: "1.6L", transmision: "Automática CVT", combustible: "Gasolina", color: "Gris", puertas: 4, potencia: "122 HP" },
+    { marca: "Nissan", modelo: "Altima", año: 2021, precio: 17000, imagen2: "/modeloU/3nissanaltima.png", imagen: "/modelos/3nissanaltimaLateral.png", motor: "1.6L", transmision: "Automática CVT", combustible: "Gasolina", color: "Gris", puertas: 4, potencia: "122 HP" },
 
-    { marca: "Mazda", modelo: "Mazda 3", año: 2022, precio: 21000, imagen: "/modelos/4Mazda3lateral.png", motor: "2.0L Skyactiv", transmision: "Automática 6 vel.", combustible: "Gasolina", color: "Azul", puertas: 4, potencia: "155 HP" },
-    { marca: "Mazda", modelo: "Mazda CX-5", año: 2022, precio: 21000, imagen: "/modelos/4MazdaCX-5lateral.png", motor: "2.0L Skyactiv", transmision: "Automática 6 vel.", combustible: "Gasolina", color: "Azul", puertas: 4, potencia: "155 HP" },
+    { marca: "Mazda", modelo: "Mazda 3", año: 2022, precio: 21000, imagen2: "/modeloU/4Mazda3.png", imagen: "/modelos/4Mazda3lateral.png", motor: "2.0L Skyactiv", transmision: "Automática 6 vel.", combustible: "Gasolina", color: "Azul", puertas: 4, potencia: "155 HP" },
+    { marca: "Mazda", modelo: "Mazda CX-5", año: 2022, precio: 21000, imagen2: "/modeloU/4MazdaCX-5.png", imagen: "/modelos/4MazdaCX-5lateral.png", motor: "2.0L Skyactiv", transmision: "Automática 6 vel.", combustible: "Gasolina", color: "Azul", puertas: 4, potencia: "155 HP" },
 
-    { marca: "Honda", modelo: "Civic", año: 2021, precio: 19000, imagen: "/modelos/5CivicLateral.png", motor: "2.0L", transmision: "CVT", combustible: "Gasolina", color: "Negro", puertas: 4, potencia: "158 HP" },
-    { marca: "Honda", modelo: "Accord", año: 2021, precio: 19000, imagen: "/modelos/5HondaAccordLateral.png", motor: "2.0L", transmision: "CVT", combustible: "Gasolina", color: "Negro", puertas: 4, potencia: "158 HP" },
+    { marca: "Honda", modelo: "Civic", año: 2021, precio: 19000, imagen2: "/modeloU/5Civic.png", imagen: "/modelos/5CivicLateral.png", motor: "2.0L", transmision: "CVT", combustible: "Gasolina", color: "Negro", puertas: 4, potencia: "158 HP" },
+    { marca: "Honda", modelo: "Accord", año: 2021, precio: 19000, imagen2: "/modeloU/5HondaAccord.png", imagen: "/modelos/5HondaAccordLateral.png", motor: "2.0L", transmision: "CVT", combustible: "Gasolina", color: "Negro", puertas: 4, potencia: "158 HP" },
 
-    { marca: "Lexus", modelo: "IS 300", año: 2022, precio: 38000, imagen: "/modelos/6LexusIS 300Lateral.png", motor: "2.0L Turbo", transmision: "Automática 8 vel.", combustible: "Gasolina", color: "Blanco", puertas: 4, potencia: "241 HP" },
-    { marca: "Lexus", modelo: "RX 350", año: 2022, precio: 38000, imagen: "/modelos/6LexusLSRX 350LATERAL.png", motor: "2.0L Turbo", transmision: "Automática 8 vel.", combustible: "Gasolina", color: "Blanco", puertas: 4, potencia: "241 HP" },
+    { marca: "Lexus", modelo: "IS 300", año: 2022, precio: 38000, imagen2: "/modeloU/6LexusIS 300.png", imagen: "/modelos/6LexusIS 300Lateral.png", motor: "2.0L Turbo", transmision: "Automática 8 vel.", combustible: "Gasolina", color: "Blanco", puertas: 4, potencia: "241 HP" },
+    { marca: "Lexus", modelo: "RX 350", año: 2022, precio: 38000, imagen2: "/modeloU/6LexusLSRX 350.png", imagen: "/modelos/6LexusLSRX 350LATERAL.png", motor: "2.0L Turbo", transmision: "Automática 8 vel.", combustible: "Gasolina", color: "Blanco", puertas: 4, potencia: "241 HP" },
 
-    { marca: "Lamborghini", modelo: "Aventador", año: 2021, precio: 200000, imagen: "/modelos/7LGAventadorLateral.png", motor: "5.2L V10", transmision: "Automática 7 vel.", combustible: "Gasolina", color: "Amarillo", puertas: 2, potencia: "610 HP" },
-    { marca: "Lamborghini", modelo: "Huracán", año: 2021, precio: 200000, imagen: "/modelos/7LGHuracánLateral.png", motor: "5.2L V10", transmision: "Automática 7 vel.", combustible: "Gasolina", color: "Amarillo", puertas: 2, potencia: "610 HP" },
+    { marca: "Lamborghini", modelo: "Aventador", año: 2021, precio: 200000, imagen2: "/modeloU/7LGAventador.png", imagen: "/modelos/7LGAventadorLateral.png", motor: "5.2L V10", transmision: "Automática 7 vel.", combustible: "Gasolina", color: "Amarillo", puertas: 2, potencia: "610 HP" },
+    { marca: "Lamborghini", modelo: "Huracán", año: 2021, precio: 200000, imagen2: "/modeloU/7LGHuracán.png", imagen: "/modelos/7LGHuracánLateral.png", motor: "5.2L V10", transmision: "Automática 7 vel.", combustible: "Gasolina", color: "Amarillo", puertas: 2, potencia: "610 HP" },
 
-    { marca: "BMW", modelo: "Serie 3", año: 2021, precio: 33000, imagen: "/modelos/8BMWSerie 3LATERAL.png", motor: "2.0L Turbo", transmision: "Automática 8 vel.", combustible: "Gasolina", color: "Gris", puertas: 4, potencia: "255 HP" },
-    { marca: "BMW", modelo: "X5", año: 2019, precio: 50000, imagen: "/modelos/8BMWX5LATERAL.png", motor: "3.0L", transmision: "Automática 8 vel.", combustible: "Gasolina", color: "Negro", puertas: 5, potencia: "335 HP" }
+    { marca: "BMW", modelo: "Serie 3", año: 2021, precio: 33000, imagen2: "/modeloU/8BMWSerie 3.png", imagen: "/modelos/8BMWSerie 3LATERAL.png", motor: "2.0L Turbo", transmision: "Automática 8 vel.", combustible: "Gasolina", color: "Gris", puertas: 4, potencia: "255 HP" },
+    { marca: "BMW", modelo: "X5", año: 2019, precio: 50000, imagen2: "/modeloU/8BMWX5.png", imagen: "/modelos/8BMWX5LATERAL.png", motor: "3.0L", transmision: "Automática 8 vel.", combustible: "Gasolina", color: "Negro", puertas: 5, potencia: "335 HP" }
   ];
 
   // filtrar por la marca recibida (case-insensitive)
